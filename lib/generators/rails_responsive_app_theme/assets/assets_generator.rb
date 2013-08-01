e7a7d3952631303d1d42c7aae0365de3234a7ced
kannan S <kannanhudson@gmail.com>
2013-08-01 14:26:15 +0530

    assets generations

--- lib/generators/rails_responsive_app_theme/assets/assets_generator.rb
+++ lib/generators/rails_responsive_app_theme/assets/assets_generator.rb
@@ -2,7 +2,6 @@ module RailsResponsiveAppTheme
   class AssetsGenerator < Rails::Generators::Base
     desc "Copy the --theme stylesheets to your rails application assets path, so you can customize them"
     source_root File.expand_path('../templates', __FILE__)
-    class_option :theme,        :type => :string,   :default => :default,   :desc => 'Specify the layout theme to be copied'
 
     def copy_stylesheets
       copy_file "main.css"          , "app/assets/theme/main.css"
@@ -10,11 +9,11 @@ module RailsResponsiveAppTheme
       copy_file "bootstrap.css"     , "app/assets/theme/bootstrap.css"
       copy_file "responsive.css"    , "app/assets/responsive.css"
 
-      directory "#{options.theme}"  , "app/assets/stylesheets/themes/#{options.theme}"
+#      directory "#{options.theme}"  , "app/assets/stylesheets/themes/#{options.theme}"
     end
 
     def copy_images
-      directory "images/responsive/themes/#{options.theme}"      , "app/assets/images/responsive/themes/#{options.theme}"
+ #     directory "images/responsive/themes/#{options.theme}"      , "app/assets/images/responsive/themes/#{options.theme}"
     end
 
   end
