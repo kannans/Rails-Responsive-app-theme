8dd16c4cd269c5a798d3ad0baca52d9ecc933e31
kannan S <kannanhudson@gmail.com>
2013-08-01 14:16:31 +0530

    layout css updated

--- lib/generators/rails_responsive_app_theme/assets/assets_generator.rb
+++ lib/generators/rails_responsive_app_theme/assets/assets_generator.rb
@@ -1,11 +1,14 @@
 module RailsResponsiveAppTheme
   class AssetsGenerator < Rails::Generators::Base
     desc "Copy the --theme stylesheets to your rails application assets path, so you can customize them"
-    source_root File.expand_path('../../../../../app/assets/', __FILE__)
+    source_root File.expand_path('../templates', __FILE__)
     class_option :theme,        :type => :string,   :default => :default,   :desc => 'Specify the layout theme to be copied'
 
     def copy_stylesheets
-      copy_file "main.css"          , "app/assets/main.css"
+      copy_file "main.css"          , "app/assets/theme/main.css"
+      copy_file "bg.css"            , "app/assets/theme/customize.css"
+      copy_file "bootstrap.css"     , "app/assets/theme/bootstrap.css"
+      copy_file "bootstrap-responsive.min.css", "app/assets/bootstrap-responsive.min.css"
 
       directory "responsive/themes/#{options.theme}" , "app/assets/stylesheets/responsive/themes/#{options.theme}"
     end
